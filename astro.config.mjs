33b867b36a523967385f5b2c21cd8630fe983977
niusha-s <newshansedgh2018@gmail.com>
2026-09-19 00:28:45 +0330

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,23 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://niushasedgh.me',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [
     mdx({
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex]
-    })
+    }),
+    sitemap()
   ]
 });
